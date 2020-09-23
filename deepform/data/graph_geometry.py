0f4f9bb39a3924e1aff1f6d961795f4b2b9b0b71
daniel <daniel.fennelly@gmail.com>
2020-09-23 15:02:48 -0700

    Add basic adjacench matrix function for tokens with test

--- deepform/data/graph_geometry.py
+++ deepform/data/graph_geometry.py
@@ -1,22 +1,34 @@
 import numpy as np
 import numpy.ma as ma
+<<<<<<< HEAD
 import scipy.sparse as sparse
+=======
+>>>>>>> 45dcc52... Add basic adjacench matrix function for tokens with test
 
 
 def document_edges(tokens, relative_tolerance=0.01):
     """"""
+<<<<<<< HEAD
     N = len(tokens)
 
     # For now we compute alignment of text tokens based on their lower left corner.
     dX = np.subtract.outer(tokens["x0"].to_numpy(), tokens["x0"].to_numpy())
     dY = np.subtract.outer(tokens["y1"].to_numpy(), tokens["y1"].to_numpy())
     page_mask = np.not_equal.outer(tokens["page"].to_numpy(), tokens["page"].to_numpy())
+=======
+
+    # For now we compute alignment of text tokens based on their lower left corner.
+    dX = np.subtract.outer(tokens["x0"].to_numpy(), tokens["x0"].to_numpy())
+
+    dY = np.subtract.outer(tokens["y1"].to_numpy(), tokens["y1"].to_numpy())
+>>>>>>> 45dcc52... Add basic adjacench matrix function for tokens with test
 
     D = np.abs(dX) + np.abs(dY)
     V_sim = dY / D
     H_sim = dX / D
 
     dX_h_aligned = ma.masked_where(
+<<<<<<< HEAD
         np.logical_or(
             page_mask,
             np.logical_not(np.isclose(np.abs(H_sim), 1, rtol=relative_tolerance)),
@@ -48,3 +60,42 @@ def document_edges(tokens, relative_tolerance=0.01):
             adjacency[bottom_max[i], i] = True
 
     return adjacency.tocoo()
+=======
+        np.logical_not(np.isclose(np.abs(H_sim), 1, rtol=relative_tolerance)), dX
+    )
+    dY_v_aligned = ma.masked_where(
+        np.logical_not(np.isclose(np.abs(V_sim), 1, rtol=relative_tolerance)), dY
+    )
+
+    # TODO: Integrate filtering for direct neighbors
+
+    # right_masked = ma.masked_where(np.less(dX_h_aligned, 0), dX_h_aligned)
+    # test_right = np.argmin(right_masked, axis=0)
+    #
+    # print(right_masked)
+    # print(test_right)
+    # print(right_masked.shape)
+    #
+    # print(right_masked)
+    #
+    # test_left = np.argmax(
+    #     ma.masked_where(np.greater(dX_h_aligned, 0), dX_h_aligned), axis=0
+    # )
+    # test_bottom = np.argmin(
+    #     ma.masked_where(np.less(dY_v_aligned, 0), dY_v_aligned), axis=0
+    # )
+    # test_top = np.argmax(
+    #     ma.masked_where(np.greater(dY_v_aligned, 0), dY_v_aligned), axis=0
+    # )
+    #
+    # print(test_left)
+    # print(test_top)
+    # print(test_bottom)
+
+    aligned = np.logical_xor(dX_h_aligned.mask, dY_v_aligned.mask)
+    adjacency = np.zeros(D.shape)
+    adjacency[aligned] = 1
+    adjacency = np.eye(D.shape[0]) + adjacency
+
+    return adjacency
+>>>>>>> 45dcc52... Add basic adjacench matrix function for tokens with test
--- tests/test_graph_geometry.py
+++ tests/test_graph_geometry.py
@@ -5,6 +5,7 @@ from deepform.data.graph_geometry import document_edges
 
 # ASCII Art of Test Example
 #
+<<<<<<< HEAD
 # A --- B --- C
 # |  W--|--X  |
 # D -|- E -|- F
@@ -80,3 +81,32 @@ def test_different_pages():
     adjacency = document_edges(tokens_pages).todense()
     assert not adjacency[0, 1]
     assert adjacency[0, 2]
+=======
+# A ---------- B
+# |            |
+# |     E      |
+# |            |
+# C ---------- D
+
+pt_A = {"token": "A", "x0": 10, "y1": 10}
+pt_B = {"token": "B", "x0": 20, "y1": 10}
+pt_C = {"token": "C", "x0": 10, "y1": 20}
+pt_D = {"token": "D", "x0": 20, "y1": 20}
+pt_E = {"token": "E", "x0": 15, "y1": 15}
+tokens = pd.DataFrame.from_records([pt_A, pt_B, pt_C, pt_D, pt_E])
+
+
+def test_adjacency_matrix():
+    adjacency = document_edges(tokens)
+    edges = np.matrix(
+        [
+            [0, 1, 1, 0, 0],
+            [1, 0, 0, 1, 0],
+            [1, 0, 0, 1, 0],
+            [0, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+    )
+    expected = edges + np.eye(5)
+    assert (adjacency == expected).all()
+>>>>>>> 45dcc52... Add basic adjacench matrix function for tokens with test
